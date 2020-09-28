37f025a343621f8c1aa6e603367c40ffb2194b9d
Surajmal Kumar <surajkumar7252@gmail.com>
2020-09-28 09:18:13 +0530

    uc1

---
+++ uc1.java
@@ -0,0 +1,27 @@
+/*
+ * This is a Snake and Ladder Game UC 1.
+ */
+
+public class uc1 {
+
+
+	
+		
+		
+	    public static void main(String[] args) {
+	   
+	    
+		int  position=0;                                      // sets the counter and position as zero initially.
+		
+		System.out.println("YOUR POSITION AT THE START : "+ position);   //prints position at the start.
+		
+		System.out.println("Your current Position: "+ position);         //Prints the current position.
+		
+			
+		
+	
+	}
+
+}
+
+
